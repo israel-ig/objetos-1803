3b79b13b3c6a0b25a19dadbf1964985fe4d8bef0
T-101 <T-101@PC230>
2018-07-09 19:41:06 -0500

    Relojito

---
+++ JavaApplication1/src/tareas1/Principal.java
@@ -0,0 +1,28 @@
+
+package tareas1;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        //E1
+        Threadsito t1=new Threadsito();
+        t1.setName("Pancho");
+        Threadsito t2=new Threadsito();
+        t2.setName("Petra");
+        Threadsito t3=new Threadsito();
+        t3.setName("Pedro");
+        
+        Relojito reloj=new Relojito();
+        
+        //E2
+        t1.start();
+        t2.start();
+        t3.start();
+        reloj.start();
+        
+        
+        
+        
+    }
+    
+}
---
+++ JavaApplication1/src/tareas1/Threadsito.java
@@ -0,0 +1,12 @@
+
+package tareas1;
+
+public class Threadsito extends Thread{
+   
+    public void run(){
+        System.out.println("Soy un thread y me llama "+Thread.currentThread().getName());
+        
+        
+    }
+}
+
